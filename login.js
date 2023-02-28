ebaf3d88e5803dcac8a2efea8621576c760e8af5
AnithaRam0n <124025308+AnithaRam0n@users.noreply.github.com>
2023-02-28 02:27:02 -0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,14 @@
+function login()
+{
+    var user1,password1
+    user1 = document.getElementById("username").value;
+    password1 = document.getElementById("password").value;
+    if( user=="Anitha" && password=="176946" )
+    {
+        window.location = "index.html";
+    }
+    else
+    {
+        alert("Error")
+    }
+}
